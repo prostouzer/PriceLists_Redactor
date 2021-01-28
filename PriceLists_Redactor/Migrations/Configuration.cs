08a8d80239516d732f41379207ce8b244f142105
prostouzer <yamail12414@yandex.ru>
2021-01-28 15:43:02 +0500

    seeding priceLists with columns

--- PriceLists_Redactor/Migrations/Configuration.cs
+++ PriceLists_Redactor/Migrations/Configuration.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using PriceLists_Redactor.Models;
+
 namespace PriceLists_Redactor.Migrations
 {
     using System;
@@ -15,7 +18,31 @@ namespace PriceLists_Redactor.Migrations
 
         protected override void Seed(PriceLists_Redactor.Data.PriceLists_RedactorContext context)
         {
+            var column1 = new Column() {DataType = DataType.SingleLine, HeaderText = "Моя однострочная колонка"};
+            var column2 = new Column() { DataType = DataType.Bool, HeaderText = "В наличии" };
 
+            var columnsList1 = new List<Column>();
+            columnsList1.Add(column1);
+            columnsList1.Add(column2);
+
+            context.PriceLists.Add(new PriceList()
+            {
+                Columns = columnsList1,
+                Name = "Прайс-лист мебели"
+            });
+
+            var column3 = new Column() { DataType = DataType.MultiLine, HeaderText = "Моя МНОГОСТРОЧНАЯ колонка" };
+            var column4 = new Column() { DataType = DataType.Bool, HeaderText = "Кол-во на складе" };
+            
+            var columnsList2 = new List<Column>();
+            columnsList1.Add(column3);
+            columnsList1.Add(column4);
+
+            context.PriceLists.Add(new PriceList()
+            {
+                Columns = columnsList2,
+                Name = "Прайс-лист одежды"
+            });
         }
     }
 }
